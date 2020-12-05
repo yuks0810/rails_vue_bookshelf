fc025c6545fd4317bef85d2a2c2d6668e0d0ae36
yuki.nakata <dorcushopeino1@gmail.com>
2020-12-06 03:55:56 +0900

    - updateを追加 - destroyを追加

--- app/javascript/pages/BookHome.vue
+++ app/javascript/pages/BookHome.vue
@@ -14,18 +14,17 @@
       <div class="col s12 m12">
         <div class="card blue-grey darken-1">
           <div class="card-content white-text">
-            <span class="card-title">
-              【{{ bookInfo.title }}】
-            </span>
-            <div class="detail">
-              ・著者：{{ bookInfo.author }}
-            </div>
-            <div class="detail">
-              ・出版社：{{ bookInfo.publisher }}
-            </div>
-            <div class="detail">
-              ・ジャンル：{{ bookInfo.genre }}
-            </div>
+            <span class="card-title"> 【{{ bookInfo.title }}】 </span>
+            <div class="detail">・著者：{{ bookInfo.author }}</div>
+            <div class="detail">・出版社：{{ bookInfo.publisher }}</div>
+            <div class="detail">・ジャンル：{{ bookInfo.genre }}</div>
+            <div class="detail">・ジャンル：{{ bookInfo.genre }}</div>
+            <router-link :to="{ path: `/edit/${bookInfo.id}` }" class="btn"
+              >本の編集</router-link
+            >
+            <button class="btn #e53935 red darken-1" v-on:click="deleteBook(bookInfo.id)">
+              削除
+            </button>
           </div>
         </div>
       </div>
@@ -34,38 +33,49 @@
 </template>
 
 <script>
-  import axios from 'axios'
+import axios from "axios";
 
-  export default {
-    name: 'BookHome',
-    data: function() {
-      return {
-        bookInfo: {},
-        bookInfoBool: false,
-        books: [],
-      }
-    },
-    mounted: function() {
-      this.fetchBooks();
-    },
-    methods: {
-      fetchBooks() {
-        axios.get('/api/books').then((res) => {
-          for(var i = 0; i < res.data.books.length; i++) {
+export default {
+  name: "BookHome",
+  data: function () {
+    return {
+      bookInfo: {},
+      bookInfoBool: false,
+      books: [],
+    };
+  },
+  mounted: function () {
+    this.fetchBooks();
+  },
+  methods: {
+    fetchBooks() {
+      axios.get("/api/books").then(
+        (res) => {
+          for (var i = 0; i < res.data.books.length; i++) {
             this.books.push(res.data.books[i]);
           }
-        }, (error) => {
+        },
+        (error) => {
           console.log(error);
-        });
-      },
-      setBookInfo(id){
-        axios.get(`api/books/${id}.json`).then(res => {
-          this.bookInfo = res.data;
-          this.bookInfoBool = true;
-        });
-      }
-    }
-  }
+        }
+      );
+    },
+    setBookInfo(id) {
+      axios.get(`api/books/${id}.json`).then((res) => {
+        this.bookInfo = res.data;
+        this.bookInfoBool = true;
+      });
+    },
+    deleteBook(id) {
+      axios.delete(`/api/books/${id}`).then((res) => {
+        this.books = [];
+        this.bookInfo = "";
+        this.bookInfoBool = false;
+        this.fetchBooks();
+      });
+    },
+  },
+};
 </script>
 
 <style scoped></style>
