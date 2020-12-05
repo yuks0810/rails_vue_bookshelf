e1ba636e188ee15223182fe5269389a2485a1d2d
yuki.nakata <dorcushopeino1@gmail.com>
2020-12-05 16:29:41 +0900

    ファイル構成修正

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -5,18 +5,18 @@
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
+  export default {
+    data: function () {
+      return {
+        message: "Hello Vue!"
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+  p {
+    font-size: 2em;
+    text-align: center;
+  }
 </style>
